ee62d252f16bb8129342cc85340b44ed8abf66a2
AZch <antonzhuikov@gmail.com>
2019-08-31 17:15:49 +0700

    Feature: update component lastConnections

--- client/components/lastConnections.js
+++ client/components/lastConnections.js
@@ -1,19 +1,40 @@
 const React = require('react');
 const Connection = require('connection');
+const StandartQuestions = require('../questions/standart');
 
 class lastConnections extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            listConn: []
+            listConn: [],
+            page: 1,
+            status: '',
         };
     }
 
     seeNextPage(event) {
         event.preventDefault();
-    }
-
-    renderConn(conn) {
+        const { page, listConn } = this.state;
+        const newPage = page + 1;
+        StandartQuestions.getData('?page=' + newPage)
+            .then((result) => {
+                if (result.error === undefined) {
+                    let newConn = lastConnections;
+                    newConn.push(result.connections);
+                    this.setState({
+                        listConn: newConn,
+                        status: 'success load page',
+                    });
+                } else {
+                    this.setState({
+                       status: 'cant load page connections'
+                    });
+                }
+            }).catch((error) => {
+                this.setState({
+                    status: 'cant get page connections'
+                });
+            });
 
     }
 
